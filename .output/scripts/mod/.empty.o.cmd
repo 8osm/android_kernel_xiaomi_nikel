1ad259622f5fdb47875b62b0ac84de3e80921102
8osm <zareichuk@yandex.ua>
2021-01-31 22:22:38 +0200

    Added pregenerated dts

--- .output/scripts/mod/.empty.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_scripts/mod/empty.o := aarch64-alpine-linux-musl-gcc -Wp,-MD,scripts/mod/.empty.o.d -nostdinc -isystem /usr/lib/gcc/aarch64-alpine-linux-musl/10.2.1/include -I../arch/arm64/include -Iarch/arm64/include/generated  -I../include -Iinclude -I../arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I../include/uapi -Iinclude/generated/uapi -include ../include/linux/kconfig.h  -I../scripts/mod -Iscripts/mod -D__KERNEL__ -mlittle-endian -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -DMEIZU_M80 -mgeneral-regs-only -fno-pic -fno-delete-null-pointer-checks -O2 -Werror=frame-larger-than=1 -Wframe-larger-than=1400 -fstack-protector-strong -Wno-unused-but-set-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time    -D"KBUILD_STR(s)=#s" -D"KBUILD_BASENAME=KBUILD_STR(empty)"  -D"KBUILD_MODNAME=KBUILD_STR(empty)" -c -o scripts/mod/empty.o ../scripts/mod/empty.c
-
-source_scripts/mod/empty.o := ../scripts/mod/empty.c
-
-deps_scripts/mod/empty.o := \
-
-scripts/mod/empty.o: $(deps_scripts/mod/empty.o)
-
-$(deps_scripts/mod/empty.o):
